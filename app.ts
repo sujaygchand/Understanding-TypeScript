ed46648f780e60d4ec1441180f1c2237ef5b5c4e
Sujay Chand <sujaygchand@gmail.com>
2021-08-19 19:05:17 +1200

    Added tuple of role to person

--- app.ts
+++ app.ts
@@ -1,22 +1,26 @@
-/*const person: {
+const person: {
     name : string;
     age: number;
     hobbies : string[];
+    role: [number, string];
 } = {
     name: "Sujay",
     age: 25,
-    hobbies: [ 'MMA', "Gaming"]
-}*/
+    hobbies: [ 'MMA', "Gaming", "Cooking", "Coding"],
+    role: [3, "Admin"]
+}
 
+/*
 const person  = {
     name: "Sujay",
     age: 25,
     hobbies: [ 'MMA', "Gaming"]
 };
+*/
 
-console.log(`${person.name} is ${person.age} years old`);
+console.log(`${person.name} is ${person.age} years old with role of ${person.role[1]} with value of ${person.role[0]}`);
 
 for (const hobby of person.hobbies) 
 {
-    console.log(hobby.toUpperCase());
+    console.log(hobby.toLowerCase());
 }
