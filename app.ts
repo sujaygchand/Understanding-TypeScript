8e3b625ce03bad100aba13f0ebee439d21b474eb
Sujay Chand <sujaygchand@gmail.com>
2021-08-19 18:34:37 +1200

    Learning object type

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log("Woop woop woomp...");
+/*const person: {
+    name : string;
+    age: number;
+} = {
+    name: "Sujay",
+    age: 25
+}*/
+
+var person = {
+    name: "Sujay",
+    age: 25
+};
+
+console.log(`${person.name} is ${person.age} years old`);
