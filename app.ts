79a13ecd4689a54629f8ed876514f5cb80d6c7dd
Sujay Chand <sujaygchand@gmail.com>
2021-08-19 19:16:09 +1200

    Added enum

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
+enum Role {Reader, Commenter, Editor};
+
 const person: {
     name : string;
     age: number;
     hobbies : string[];
-    role: [number, string];
+    role: [Role, boolean];
 } = {
     name: "Sujay",
     age: 25,
     hobbies: [ 'MMA', "Gaming", "Cooking", "Coding"],
-    role: [3, "Admin"]
+    role: [Role.Editor, true]
 }
 
 /*
@@ -18,7 +20,7 @@ const person  = {
 };
 */
 
-console.log(`${person.name} is ${person.age} years old with role of ${person.role[1]} with value of ${person.role[0]}`);
+console.log(`${person.name} is ${person.age} years old with role of ${person.role[0]} and is active: ${person.role[1]}`);
 
 for (const hobby of person.hobbies) 
 {
